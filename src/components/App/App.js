67ca19afdc4519c418a67e2cba824b8598f5011f
ha6icht <rolf.karlen@gmail.com>
2020-05-25 21:00:31 +0200

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,15 +5,104 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      searchResults: [{
+        name: 'Yellow Submarine',
+        artist: 'The Beatles',
+        album: 'Revolver',
+        id: 1
+      },{
+        name: 'Tiny Dancer',
+        artist: 'Elton John',
+        album: 'Madman Across The Water',
+        id: 2
+      },{
+        name: 'Yellow Submarine',
+        artist: 'Sweet Little Band',
+        album: 'Beatles Para Bebês Vol. 2',
+        id: 3
+      },{
+        name: 'Yellow Submarine',
+        artist: 'Beat Bugs',
+        album: 'WhoSampled',
+        id: 4
+      },{
+        name: 'Tiny Dancer',
+        artist: 'Tim McGraw',
+        album: 'Love Story',
+        id: 5
+      }],
+      playlistName: 'New Playlist',
+      playlistTracks: [{
+        name: 'Stronger',
+        artist: 'Britney Spears',
+        album: 'Oops!.. I Did It Again',
+        id: 6
+        },{
+          name: 'So Emontional',
+          artist: 'Whitney Houston',
+          album: 'Whitney',
+          id: 7
+        },{
+          name: 'It\'s Not Right But It\'s Okay',
+          artist: 'Whitney Houston',
+          album: 'My Love Is Your Love',
+          id: 8
+        }
+
+      ]
+    }
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+  }
+  addTrack(track){
+    if(this.state.playlistTracks.find
+      (savedTrack => savedTrack.id === track.id)) return;
+    else{
+      let playlistTracks = this.state.playlistTracks;
+      playlistTracks.push(track);
+      this.setState({
+        playlistTracks: playlistTracks
+      })
+    }      
+  }
+  removeTrack(track){
+    if(this.state.playlistTracks.find
+      (savedTrack => savedTrack.id === track.id)){
+      let playlistTracks = this.state.playlistTracks.filter(playlistTrack => playlistTrack.id !== track.id)
+      this.setState({
+        playlistTracks: playlistTracks
+      });
+    }
+    else return;
+  }
+  updatePlaylistName(name){
+    this.setState({
+      playlistName: name
+    })
+  }
+  savePlaylist(){
+    const uri = this.state.playlistTracks;
+    const trackUris = uri;
+  }
   render(){
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
-        <div class="App">
+        <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
-            <Playlist />
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
+            <Playlist playlistName={this.state.playlistName} 
+              onRemove={this.removeTrack} 
+              playlistTracks={this.state.playlistTracks}
+              onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist}
+              />
           </div>
         </div>
       </div>
