8e3f57d3621ca9c1e7db9dd498f436f1206fa351
ha6icht <rolf.karlen@gmail.com>
2020-05-25 21:00:51 +0200

    Update Playlist.js

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -3,15 +3,22 @@ import './Playlist.css';
 import TrackList from '../TrackList/TrackList'
 
 class Playlist extends React.Component{
-  render(){
-    return (
-        <div className="Playlist">
-            <input defaultValue={'New Playlist'}/>
-            <TrackList />
-            <button className="Playlist-save">SAVE TO SPOTIFY</button>
-        </div>
-    );
-  }
+    constructor(props){
+        super(props);
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+    handleNameChange(e){
+        this.props.onNameChange(e.target.value);
+    }
+    render(){
+        return (
+            <div className="Playlist">
+                <input defaultValue={'New Playlist'} onChange={this.handleNameChange}/>
+                <TrackList tracks={this.props.playlistTracks} isRemoval={false} onRemove={this.props.onRemove}/>
+                <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
+            </div>
+        );
+    }
 }
 
 export default Playlist;
