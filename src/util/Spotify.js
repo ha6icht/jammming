af3e7d33e8ac092e9fb454ffe726e45cc2c44ba4
ha6icht <rolf.karlen@gmail.com>
2020-07-20 18:13:25 +0200

    Update Spotify.js

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,7 +1,3 @@
-//import React from 'react';
-//import './Playlist.css';
-//import TrackList from '../TrackList/TrackList'
-
 const CLIENT_ID = "0bc1fd2087ab43f5b77e4186465a2ec2";
 const CLIENT_ID_BASE64 = "MGJjMWZkMjA4N2FiNDNmNWI3N2U0MTg2NDY1YTJlYzI=";
 const CLIENT_SECRET = "f7b4a87ae933404fad791c523e8a1350";
@@ -17,40 +13,18 @@ let Spotify = {
         if(accessToken.length > 0 ) return accessToken;
         else if(window.location.href.match(accessTokenReg) && 
                 window.location.href.match(expiresInReg)){
-            //console.log('In else if')
             const accessTokenMatch = window.location.href.match(accessTokenReg);
             const expiresInMatch = window.location.href.match(expiresInReg);
             accessToken = accessTokenMatch[1];
             let expiresIn = expiresInMatch[1];
-            console.log(accessToken)
-            window.setTimeout(() => accessToken = '', expiresIn * 1000);
-            window.history.pushState('Access Token', null, '/');
-            console.log(accessToken, expiresIn)
-        }
-        else{ window.location = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&scope=playlist-modify-public&redirect_uri=${REDIRECT_URI}`;
-            /*return fetch(`https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&scope=playlist-modify-public&redirect_uri=${REDIRECT_URI}`
-            ).then( response =>{ 
-                        return response.json();
-                    })/*.then(jsonResponse =>{
-                        return jsonResponse;
-                    })*/
-        }
-        //this.setAccessToken();
-
-    },
-    setAccessToken(){
-        const accessTokenReg = "/access_token=([^&]*)/";
-        const expiresInReg = "/expires_in=([^&]*)/";
-        if(window.location.href.match(accessTokenReg) && 
-                window.location.href.match(expiresInReg)){
-            accessToken = window.location.href.match(accessTokenReg);
-            let expiresIn = window.location.href.match(expiresInReg);
             window.setTimeout(() => accessToken = '', expiresIn * 1000);
             window.history.pushState('Access Token', null, '/');
         }
-        console.log(accessToken);
+        else window.location = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&scope=playlist-modify-public&redirect_uri=${REDIRECT_URI}`;
+    
     },
     search(searchTerm){
+        accessToken = this.getAccessToken();
         console.log('In search()',accessToken);
         return fetch(`https://cors-anywhere.herokuapp.com/
             https://api.spotify.com/v1/search?type=track&q=${searchTerm}`, {
@@ -79,20 +53,43 @@ let Spotify = {
             });
     },
     savePlaylist(playlistName, trackURIs){
-        if(playlistName !== '' && trackURIs !== '');
-        let accessTokenUser = accessToken;
+        if(playlistName !== '' && trackURIs !== '') return;
+        let token = this.getAccessToken();
         let headers = {
-            Authorization: `Bearer ${accessTokenUser}`
+            Authorization: `Bearer ${token}`
         };
-        let userID = '';
+        let userId = '';
+        let playlistId = '';
         fetch(`https://cors-anywhere.herokuapp.com/
             https://api.spotify.com/v1/me`, {
             headers: headers
-        }).then(response =>{
-            return response.json();
-        }).then(jsonResponse =>{
-            if(jsonResponse.id) userID = jsonResponse.id;
-        });
+            }).then(response =>{
+                return response.json();
+            }).then(jsonResponse =>{
+                if(jsonResponse.id) userId = jsonResponse.id;
+            }).then(() => {
+                fetch(`https://api.spotify.com/v1/users/${userId}/playlists`,{
+                method: 'POST',
+                headers: headers,
+                body: JSON.stringify({name: playlistName}),
+                json: true
+                }).then(response => { 
+                    return response.json()
+                }).then(jsonResponse => {
+                playlistId = jsonResponse.id;
+                }).then(() => {
+                fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`,{
+                method: 'POST',
+                headers: headers,
+                body: JSON.stringify({uris: trackURIs}),
+                json: true
+                }).then(response => { 
+                    return response.json()
+                }).then(jsonResponse => {
+                    playlistId = jsonResponse.id;
+                })
+            })
+          });
         
 
     }
